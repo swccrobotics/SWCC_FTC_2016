401146544c2dbe7e691f6300831294f59de4c3ac
swccrobotics <swccrobotics@gmail.com>
2016-11-06 15:12:25 -0500

    Added to test out ball launcher.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LauncherCheck.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/LauncherCheck.java
@@ -42,23 +42,26 @@ public class LauncherCheck extends LinearOpMode {
 
             // ----------SECTION FOR BALL LAUNCHER----------
             // Use Gamepad 1 Right Bumper and Trigger to increase/decrease the launcher speed.
-            if (gamepad1.right_bumper)
+            if (gamepad1.right_bumper) {
                 LaunchSpeedIdx += 1;
-            else if (gamepad1.right_trigger > 0.5)
+            } else if (gamepad1.right_trigger > 0.5) {
                 LaunchSpeedIdx -= 1;
+            }
             // Keep the LaunchSpeedIdx within the bounds of the array.
             LaunchSpeedIdx = Range.clip(LaunchSpeedIdx, 0, LauncherSpeed.length-1); // Length - 1 because array is zero-indexed.
 
             // Use Gamepad 1 "Y" button to enable/disable the launcher.
-            if (gamepad1.y)
+            if (gamepad1.y) {
                 LauncherState = !LauncherState;
+            }
 
-            if (LauncherState)
+            if (LauncherState) {
                 robot.BallLauncherLeft.setPower(LauncherSpeed[LaunchSpeedIdx]);
                 robot.BallLauncherRight.setPower(LauncherSpeed[LaunchSpeedIdx]);
-            else
+            } else {
                 robot.BallLauncherLeft.setPower(0.0);
                 robot.BallLauncherRight.setPower(0.0);
+            }
 
             // Send telemetry message to signify robot running;
             telemetry.addData("BallLauncherPower:",  "Ball Launcher Power = %.2f", LauncherSpeed[LaunchSpeedIdx]);
